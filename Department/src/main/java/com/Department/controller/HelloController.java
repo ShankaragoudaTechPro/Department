ad60a2aac870dab1f7adbd53cdfa65a866f83bbe
Shankargouda <shankrupagad@gmail.com>
2025-09-06 03:03:44 -0700

    Updated code with controller

---
+++ Department/src/main/java/com/Department/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.Department.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hellos")
+public class HelloController {
+}
